34a9de776760b47fafa04f0b0faddac9cbd2ca01
Huglaire <pollon.hugo@gmail.com>
2026-04-12 11:51:36 +0200

    Ajout authentification rôles dans le routage

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,18 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize;
     }
 }
+
+/*
+Explication tableau des rôles :
+[] : accessible à tous
+["disconnected"] : accessible uniquement aux utilisateurs non connectés
+["client"] : accessible uniquement aux clients connectés
+["admin"] : accessible uniquement aux administrateurs connectés
+["client", "admin"] : accessible uniquement aux clients et administrateurs connectés
+*/
